ee677a109e4555c215018fbe9cc137216dbef543
IliaSuponeff <suponeff.i@gmail.com>
2023-11-05 23:45:25 +0300

    Update 'Post' model in 'blog' application. Add string view and some functions

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,8 +1,28 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
 	title = models.CharField(max_length=200)
 	description = models.TextField()
 	link = models.CharField(max_length=200)
-	publication_date = models.DateTimeField()
+	publication_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self) -> str:
+		args = [
+			f"id={self.id}",
+			f"title={self.title}",
+			f"descripton={self.description[:50]}",
+			f"link={self.link}",
+			f"date={self.publication_date}"
+		]
+		return f"<{self.__class__.__name__}: {' '.join(args)}>"
+	
+	def __repr__(self) -> str:
+		return self.__str__()
+	
+	def was_published_recently(self)-> bool:
+		return self.publication_date >= timezone.now() - datetime.timedelta(days=1)
+	
