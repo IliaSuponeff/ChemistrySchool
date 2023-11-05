c53e6eb8159e27c711690c9efe77cbd18f9ee031
IliaSuponeff <suponeff.i@gmail.com>
2023-11-05 20:48:26 +0300

    Add model 'Post' to 'blog' application

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    link = models.CharField(max_length=200)
+    publication_date = models.DateTimeField()
