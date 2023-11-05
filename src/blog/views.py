cd27c7e0191469165f0c308755f07c76c3ae3f7c
IliaSuponeff <suponeff.i@gmail.com>
2023-11-05 19:43:28 +0300

    Add 'blog' application to django

---
+++ src/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+"""
+TODO: Add view of add/edit post
+TODO: Add view of representation of posts at db
+"""
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
